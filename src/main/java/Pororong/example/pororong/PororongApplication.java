dcce826d8ad1e33f86dde2c558702e3686948b86
RyuJiye <coomicuma@naver.com>
2024-08-01 16:35:24 +0900

    Initial commit

---
+++ src/main/java/Pororong/example/pororong/PororongApplication.java
@@ -0,0 +1,13 @@
+package Pororong.example.pororong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PororongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PororongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Pororong/example/pororong/PororongApplicationTests.java
@@ -0,0 +1,13 @@
+package Pororong.example.pororong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PororongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
